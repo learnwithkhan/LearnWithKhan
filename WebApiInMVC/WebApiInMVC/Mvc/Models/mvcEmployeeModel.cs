f832739f1fe41d4e9641752f1ada2648849c84c0
learningwithkhan <learningkhan@outlook.com>
2019-12-16 00:10:40 -0500

    second commit

---
+++ WebApiInMVC/WebApiInMVC/Mvc/Models/mvcEmployeeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc.Models
+{
+    public class mvcEmployeeModel
+    {
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage="This Field is Required")]
+        public string Name { get; set; }
+        public string Position { get; set; }
+       
+        public Nullable<int> Age { get; set; }
+
+        [Required(ErrorMessage = "Only Numeric Value is Required")]
+        [DataType(DataType.Currency)]
+        public Nullable<int> Salary { get; set; }
+        public Nullable<double>Itex { get; set; }
+        public Nullable<double>EI { get; set; }
+        public Nullable<double>CPP { get; set; }
+        public Nullable<int> Additions { get; set; }
+
+
+
+    }
+}
---
+++ WebApiInMVC/mvc-ajax-example-master/mvc-ajax-example-master/AjaxExample/Controllers/BookController.cs
@@ -0,0 +1,87 @@
+﻿using AjaxExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace AjaxExample.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookAjaxEntities _context = new BookAjaxEntities();
+
+        BookAjaxEntities md = new BookAjaxEntities();
+
+        public BookController()
+        {
+           md = new BookAjaxEntities();
+        }
+
+        
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public ActionResult GetBooks()
+        {
+            var books = _context.Books.ToList();
+
+            return Json(books, JsonRequestBehavior.AllowGet);
+        }
+
+        
+        public ActionResult Get(int id)
+        {
+            var book = md.Books.ToList().Find(m => m.Id == id);
+            return Json(book, JsonRequestBehavior.AllowGet);
+        }
+
+        
+       
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "Id")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                md.Books.Add(book);
+                md.SaveChanges();
+            }
+
+            return Json(book, JsonRequestBehavior.AllowGet);
+        }
+
+      
+        [HttpPost]
+        public ActionResult Update(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(book).State = EntityState.Modified;
+                _context.SaveChanges();
+
+            }
+
+            return Json(book, JsonRequestBehavior.AllowGet);
+        }
+
+      
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var book = _context.Books.ToList().Find(m => m.Id == id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+
+            return Json(book, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
